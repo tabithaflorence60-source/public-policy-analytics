0166233f07d54d9e2f208ab5754e08845424c2b8
tabithaflorence60-source <tabithaflorence60@gmail.com>
2026-09-07 14:35:00 -0700

    Remove placeholder R lab

--- R-Labs/Statistical-Inference.R
+++
@@ -1,6 +0,0 @@
-# Statistical Inference Lab
-#
-# Validated R exercises will be added here after the dataset,
-# test assumptions, specifications, and outputs have been checked.
-#
-# This file is intentionally code-free for now.
