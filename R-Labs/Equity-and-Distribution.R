7a681fd8b01f2b788b4403c2d6537625d400a0c2
tabithaflorence60-source <tabithaflorence60@gmail.com>
2026-09-07 14:34:43 -0700

    Remove placeholder R lab

--- R-Labs/Equity-and-Distribution.R
+++
@@ -1,6 +0,0 @@
-# Equity and Distribution Lab
-#
-# Validated R exercises will be added here after the relevant dataset,
-# variables, assumptions, and outputs have been checked.
-#
-# This file is intentionally code-free for now.
