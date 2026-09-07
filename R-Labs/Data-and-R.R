c90387f8eddf9fedd50f3dc114e9612aa01266cc
tabithaflorence60-source <tabithaflorence60@gmail.com>
2026-09-07 14:34:38 -0700

    Remove placeholder R lab

--- R-Labs/Data-and-R.R
+++
@@ -1,6 +0,0 @@
-# Data and R Lab
-#
-# Validated R exercises will be added here after they have been run
-# successfully against documented datasets and checked for reproducibility.
-#
-# This file is intentionally code-free for now.
