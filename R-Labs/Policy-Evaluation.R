8e2107a3bb92aac83016be77b4a5b15dd015e7f3
tabithaflorence60-source <tabithaflorence60@gmail.com>
2026-09-07 14:34:48 -0700

    Remove placeholder R lab

--- R-Labs/Policy-Evaluation.R
+++
@@ -1,6 +0,0 @@
-# Policy Evaluation Lab
-#
-# Validated R exercises will be added here after the evaluation design,
-# dataset structure, assumptions, and outputs have been checked.
-#
-# This file is intentionally code-free for now.
